46f8784f24a77159b4276af655de54d1b22091c6
Helder Alves <helderdek@gmail.com>
2019-08-26 21:16:22 -0300

    Atualização, fim do node começo do front

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -9,4 +9,8 @@ server.use('/api', router)
 const billingCycleServices = require('../api/billingCycle/billingCycleService')
 billingCycleServices.register(router, '/billingCycles')
 
+
+const billingSummaryService = require('../api/billingSummary/billingSummaryService')
+router.route('/billingSummary').get(billingSummaryService.getSummary)
+
 }
