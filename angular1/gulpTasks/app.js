398484e2d8c63c0489b3f4cdd2ef267132be41ff
Helder Alves <helderdek@gmail.com>
2019-08-26 21:30:15 -0300

    feito a base do gulp

---
+++ angular1/gulpTasks/app.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp')
+
+gulp.task('app', ['app.html', 'app.css', 'app.assets'])
+
+gulp.task('app.html', () => {
+
+})
+
+gulp.task('app.css', () => {
+
+})
+
+gulp.task('app.assets', () => {
+
+})
